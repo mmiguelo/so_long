c259b8eb053a9db06082539c879c13e3f3aeaf6b
Marco Alexandre Miguelote Santos <mmiguelo@c1r9s5.42porto.com>
2025-01-03 12:11:14 +0000

    ammend to fd compare to -1

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mmiguelo <mmiguelo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/03 11:30:19 by mmiguelo          #+#    #+#             */
-/*   Updated: 2025/01/03 12:04:56 by mmiguelo         ###   ########.fr       */
+/*   Updated: 2025/01/03 12:08:41 by mmiguelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	main(int argc, char **argv)
 	{
 		extension(argv[1]);
 		fd = open(argv[1], O_RDONLY);
-		if (fd = -1)
+		if (fd == -1)
 			kill (2);
 	}
 	return (0);
